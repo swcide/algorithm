399286f5a98c6c96019f8c496cbc7acb6cba653e
swcide <swcide@gmail.com>
2021-06-20 21:54:40 +0900

    '2021.06.20'

---
+++ hanghae/24_9012(괄호).py
@@ -0,0 +1,68 @@
+"""
+문제
+괄호 문자열(Parenthesis String, PS)은 두 개의 괄호 기호인 ‘(’ 와 ‘)’ 만으로 구성되어 있는 문자열이다.
+그 중에서 괄호의 모양이 바르게 구성된 문자열을 올바른 괄호 문자열(Valid PS, VPS)이라고 부른다.
+한 쌍의 괄호 기호로 된 “( )” 문자열은 기본 VPS 이라고 부른다.
+만일 x 가 VPS 라면 이것을 하나의 괄호에 넣은 새로운 문자열 “(x)”도 VPS 가 된다.
+그리고 두 VPS x 와 y를 접합(concatenation)시킨 새로운 문자열 xy도 VPS 가 된다.
+예를 들어 “(())()”와 “((()))” 는 VPS 이지만 “(()(”, “(())()))” ,
+그리고 “(()” 는 모두 VPS 가 아닌 문자열이다.
+
+여러분은 입력으로 주어진 괄호 문자열이 VPS 인지 아닌지를 판단해서 그 결과를 YES 와 NO 로 나타내어야 한다.
+
+"""
+
+"""
+예제 1                          출력
+6
+(())())                         NO
+(((()())()                      NO
+(()())((()))                    YES
+((()()(()))(((())))()           NO
+()()()()(()()())()              YES
+(()((())()(                     NO
+
+
+예제 2                          출력
+
+3
+((                              NO
+))                              NO
+())(()                          NO
+
+
+먼저 괄호를 받았을 때
+( 라면 stack에 append 해준다.
+그뒤에 )가 나오면 pop을 해준다.
+매우 간단하다.
+if stack: 문으로 
+
+"""
+
+import sys
+
+# input = sys.stdin.readline
+
+n = int(input())
+
+for _ in range(n):
+    Bracket = input()
+    stack = []
+    for i in range(len(Bracket)):
+        if Bracket[i] =="(":
+            stack.append(Bracket[i])
+        else :
+            if stack:
+                stack.pop()
+            else:
+                print("NO")
+                break
+    else:
+        if not stack:
+            print("YES")
+        else:
+            print("NO")
+
+
+
+
