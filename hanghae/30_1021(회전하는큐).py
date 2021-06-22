341fe104ef2d56e39012ca555bb90e6a505a0afc
swcide <swcide@gmail.com>
2021-06-22 19:30:13 +0900

    '20210622'

--- hanghae/30_1021(회전하는큐).py
+++ hanghae/30_1021(회전하는큐).py
@@ -37,3 +37,34 @@ a1, ..., ak가 ak, a1, ..., ak-1이 된다.
 
 
 """
+
+
+from collections import deque
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+idxs = list(map(int, input().split()))
+dq = deque([i for i in range(1, n+1)])
+
+count = 0
+for idx in idxs:
+    while True:
+        if dq[0] == idx:
+            dq.popleft()
+            break
+        else:
+            if dq.index(idx) < len(dq)/2:
+                while dq[0] != idx:
+                    dq.append(dq.popleft())
+                    count += 1
+            else:
+                while dq[0] != idx:
+                    dq.appendleft(dq.pop())
+                    count += 1
+print(count)
+
+
+"""
+나중에 알아보도록 하자...
+"""
